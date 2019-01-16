5d391f93e0b8ebdc354dc0194ce2f56d0edecb6c
Z <aerodinamicbg@gmail.com>
2019-01-16 16:58:25 +0200

    selenium

--- TestingSelenium/testingWithSelenium.py
+++ TestingSelenium/testingWithSelenium.py
@@ -1,12 +1,22 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome(executable_path = "G:/Python/TestingSelenium/chromedriver.exe")
+driver = webdriver.Chrome()
 driver.maximize_window()
-driver.get("https://www.google.com")
-driver.find_element_by_name("q").send_keys("zlatan angelov")
-driver.find_element_by_name("btnK").click()
+driver.get("http://www.seleniumhq.org")
+q = driver.find_element_by_id("q")
+print("Element with id q:")
+print(q)
 
-print(driver.title)
+q_name = driver.find_element_by_name("q")
+print("Element with name q:")
+print(q_name)
+
+class_name = driver.find_element_by_class_name("selenium-sponsors")
+print("Element with class name selenium sponsors:")
+print(class_name)
+
+heading = driver.find_element_by_xpath('//*[@id="mainContent"]/h2[1]')
+print(heading)
 
 driver.close()
 driver.quit()
