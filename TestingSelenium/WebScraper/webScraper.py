b0f485a4daf2caa0d9f443f8846b60aec0712201
Z <aerodinamicbg@gmail.com>
2019-01-19 23:54:06 +0200

    scraping front and inner pages

--- TestingSelenium/WebScraper/webScraper.py
+++ TestingSelenium/WebScraper/webScraper.py
@@ -1,31 +1,50 @@
 import lxml
 import bs4
 import requests
+from datetime import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-request = requests.get("https://www.vesti.bg/")
-
-soup = bs4.BeautifulSoup(request.text, 'lxml')
-
-titles = set([])
-links = set([])
-h2 = soup.select('.gtm-TopNews-click')
-
 def scrapeWeb():
+       request = requests.get("https://www.vesti.bg/")
+
+       soup = bs4.BeautifulSoup(request.text, 'lxml')
+
+       titles = []
+       links = []
+       h2 = soup.select('.gtm-TopNews-click')
+
        for element in h2:
               title = element.img
+              link = element['href']
               if title is not None:
-                     titles.add(title['alt'])
-                     links.add(element['href'])
+                     titles.append(title['alt'])
+                     links.append(link)
 
+       scrapeArticles(links)
+
+def scrapeArticles(links):
        for link in links:
               rq = requests.get(link)
-              article = bs4.BeautifulSoup(rq.text, 'lxml')
+              page = bs4.BeautifulSoup(rq.text, 'lxml')
+
+              #author
+              author = page.select('.author') # that returns a list of all elements under class 'author'. We expect only 1 element in return.
+              authorName = author[0].img['alt']
+
+              #article text - all the p's in class 'article-text'. We expect only 1 'article-text' element
+              article = page.select('.article-text')[0].select('p')
+              articleText = ''
+              for paragraphIndex in range(len(article)-2):
+                     para = article[paragraphIndex]
+                     paragraph = str(para)[3:-4] #to exclude <p></p>
+                     articleText = articleText + ' ' + paragraph
 
               #headline 2: 'subtitle'
-              #text: 'article-text', with all the paragraphs separated into different <p></p>
+              
+              #article time
+              articleDate = datetime.now().date()
 
-
-scheduler = BlockingScheduler()
-scheduler.add_job(scrapeWeb, 'interval', hours=4)
-scheduler.start()
+scrapeWeb()
+#scheduler = BlockingScheduler()
+#scheduler.add_job(scrapeWeb, 'interval', hours=4)
+#scheduler.start()
