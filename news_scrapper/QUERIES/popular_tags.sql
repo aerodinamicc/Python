093fa11af30a34a4942f67429124c4dec4dd02d3
aerodinamicc <aerodinamicbg@gmail.com>
2020-03-08 16:23:02 +0200

    Update popular_tags.sql

--- news_scrapper/QUERIES/popular_tags.sql
+++ news_scrapper/QUERIES/popular_tags.sql
@@ -27,48 +27,78 @@ order by 2 desc
 --- Daily popularity of different tags (ordered by views and comments)
 -----------------------
  
+CREATE TABLE IF NOT EXISTS news.popular_tags
+WITH (format='parquet', external_location='s3://testo-bucket/popular-tags') as
 WITH src as (
 select
-  link,
-  title,
-  views,
-  comments,
-  date_parse(visited_timestamp, '%Y-%m-%d %H:%i:%S') as visited_timestamp,
-  date_parse(created_timestamp, '%Y-%m-%d %H:%i:%S') as created_timestamp,
-  lag(comments) OVER (PARTITION BY link
-                    ORDER BY visited_timestamp) as prev_comments,
-  lag(views) OVER (PARTITION BY link
-                    ORDER BY visited_timestamp) as prev_views,
-  lag(date_parse(visited_timestamp, '%Y-%m-%d %H:%i:%S')) OVER (PARTITION BY link
-                    ORDER BY visited_timestamp) as prev_visited,
-  tags,
-  category,
-  
-  row_number() OVER (PARTITION BY link
-                    ORDER BY visited_timestamp) as rnk
-  from news.raw
-  where substr(created_timestamp, 1, 10) like '2020-%'
-  and url_extract_host(link) like '%news.bg'
+	link,
+	title,
+	views,
+	comments,
+	date_parse(visited_timestamp, '%Y-%m-%d %H:%i:%S') as visited_timestamp,
+	date_parse(created_timestamp, '%Y-%m-%d %H:%i:%S') as created_timestamp,
+	lag(comments) OVER (PARTITION BY link
+                    	ORDER BY visited_timestamp) as prev_comments,
+ 	lag(views) OVER (PARTITION BY link
+					 ORDER BY visited_timestamp) as prev_views,
+	lag(date_parse(visited_timestamp, '%Y-%m-%d %H:%i:%S')) OVER (PARTITION BY link
+																  ORDER BY visited_timestamp) as prev_visited,
+	trim(tags) as tags,
+	category,
+	row_number() OVER (PARTITION BY link
+					   ORDER BY visited_timestamp) as rnk,
+	partition_0 as site
+from news.raw
+	where substr(created_timestamp, 1, 10) like '2020-%'
+	and (tags is not null or trim(tags) <> '')
+  -- and url_extract_host(link) like '%.bg'
 ),
   
 daily as (select 
-  date(visited_timestamp) as date,
-  split(lower(tags), ' - ') as tags,
-case when rnk = 1 then comments
-     else comments - prev_comments END AS comments_generated,
-case when rnk = 1 then views
-     else views - prev_views END AS views_generated
+	site,
+	date(visited_timestamp) as date,
+	split(lower(tags), ' - ') as tags,
+	case when rnk = 1 then comments
+	     else comments - prev_comments END AS comments_generated,
+	case when rnk = 1 then views
+	     else views - prev_views END AS views_generated
 from src),
 
-elongated as (select date, tag, sum(comments_generated) as sum_comments, sum(views_generated) as sum_views
+total_views as (
+select 
+	site, 
+	date, 
+	sum(comments_generated) as site_sum_comments, 
+	sum(views_generated) as site_sum_views
 from daily
-cross join unnest(tags) as t(tag)
 group by 1, 2
-order by 1, 2)
+),
 
-select date, tag, sum_comments, sum_views, row_number() over (partition by date order by sum_views desc, sum_comments desc) as rank
-from elongated
--- where sum_views > 10000
--- where regexp_like(tag, '(?:(?:^евро$)|еврозона)')
-order by 1, 4 desc, 3 desc
+tag_views as (
+select 
+	d.site, 
+	d.date, 
+	tag, 
+	sum(comments_generated) as sum_comments, 
+	t.site_sum_comments, 
+	round(sum(comments_generated) * 100. / t.site_sum_comments, 1) as share_comments, 
+	sum(views_generated) as sum_views, 
+	t.site_sum_views,
+	round(sum(views_generated) * 100. / t.site_sum_views, 1) as share_views
+from daily d
+cross join unnest(tags) as t(tag)
+left join total_views t
+	on d.site = t.site and d.date = t.date
+group by 1, 2, 3, 5, 8
+order by 1, 2, 3),
 
+ranked as (
+select
+	tag_views.*,
+	row_number() over (partition by date 
+					   order by sum_views desc, sum_comments desc) as rnk
+from tag_views
+order by 1, 2, 7 desc)
+
+select *
+from ranked
